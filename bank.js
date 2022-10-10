5c33ae33fdeec1aa33d5255b42d03b2bae50cc5a
bappy-ahashan <bappyahashan@gmail.com>
2022-10-11 03:18:22 +0600

    today finished

--- bank.js
+++ bank.js
@@ -1,5 +1,22 @@
-document.getElementById('login-submit').addEventListener('click', function () {
+document.getElementById('Login-submit').addEventListener('click', function () {
+    // get user email
     const emailField = document.getElementById('user-email');
     const userEmail = emailField.value;
     console.log(userEmail);
+    // get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    console.log(userPassword);
+
+    // email & password checked
+
+    if (userEmail == 'bappy@gmail.com' && userPassword == '123') {
+
+        window.location.href = 'banking.html';
+    }
+
+    else {
+        console.log('invalid');
+    }
+
 })
