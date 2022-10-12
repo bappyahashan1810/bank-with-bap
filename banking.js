16378f98074ae6564f1c7472d620f0545011012e
bappy-ahashan <bappyahashan@gmail.com>
2022-10-12 14:51:31 +0600

    using function

--- banking.js
+++ banking.js
@@ -1,22 +1,45 @@
-// banking page
-document.getElementById('deposit-button').addEventListener('click', function () {
-
-    // get the amount deposited
-    const depositInput = document.getElementById('deposit-input');
+function getInputValue(inputValue) {
+    const depositInput = document.getElementById(inputValue);
     const newDepositText = depositInput.value;
     const newDepositValue = parseFloat(newDepositText);
-    // console.log(depositValue);
+    depositInput.value = '';
+
+    return newDepositValue;
+
+}
+
+
+
+
+
+
+// banking page
+// handle deposit
+document.getElementById('deposit-button').addEventListener('click', function () {
+
+    // const depositInput = document.getElementById('deposit-input');
+    // const newDepositText = depositInput.value;
+    // const newDepositValue = parseFloat(newDepositText);
+    const newDepositValue = getInputValue('deposit-input');
+
+    //    deposit field
 
     const depositTotal = document.getElementById('deposit-total');
     const previousDepositValue = depositTotal.innerText;
-
+    // total deposit
     const newDepositTotal = parseFloat(previousDepositValue) + newDepositValue;
     depositTotal.innerText = newDepositTotal;
 
 
-    // update account balance 
+    // update account balance with deposit
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalanceValue = parseFloat(previousBalanceText);
+    const newBalanceTotal = previousBalanceValue + newDepositValue;
 
-    depositInput.value = '';
+    balanceTotal.innerText = newBalanceTotal;
+
+    // depositInput.value = '';
 });
 
 
@@ -28,12 +51,13 @@ document.getElementById('withdraw-button').addEventListener('click', function ()
     const newWidthdrawValue = parseFloat(newWidthdrawText);
 
 
-    // set withdraw total
+    // withdraw field
 
     const withdrawTotal = document.getElementById('withdraw-total');
     const previousWithdrawText = withdrawTotal.innerText;
     const previousWithdrawValue = parseFloat(previousWithdrawText);
 
+    // total widthdraw
     const newWithdrawTotal = previousWithdrawValue + newWidthdrawValue;
     withdrawTotal.innerText = newWithdrawTotal;
 
